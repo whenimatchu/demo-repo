19d6168796777f2f75a162427ead6b4cd1e6ec8b
Matthew Ormilon <matthewormilon@gmail.com>
2025-07-14 09:12:01 +0800

    adding all coding files

---
+++ test code.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl; // Output "Hello, World!"
+    return 0; // Return 0 to indicate successful execution
+}
